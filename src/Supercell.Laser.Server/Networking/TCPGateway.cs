0eb551f39401ae4710aae17a27d4fff23f815a3f
Erder00 <108551294+Erder00@users.noreply.github.com>
2024-06-14 20:27:23 +0200

    shows client ip on connect and disconnect + fixed formatting and stufffff

--- src/Supercell.Laser.Server/Networking/TCPGateway.cs
+++ src/Supercell.Laser.Server/Networking/TCPGateway.cs
@@ -1,125 +1,105 @@
-﻿namespace Supercell.Laser.Server.Networking
-{
-    using Supercell.Laser.Server.Networking.Session;
-    using System.Net;
-    using System.Net.Sockets;
+﻿namespace Supercell.Laser.Server.Networking {
+  using Supercell.Laser.Server.Networking.Session;
+  using System.Net;
+  using System.Net.Sockets;
 
-    public static class TCPGateway
-    {
-        private static List<Connection> ActiveConnections;
+  public static class TCPGateway {
+    private static List < Connection > ActiveConnections;
 
-        private static Socket Socket;
-        private static Thread Thread;
+    private static Socket Socket;
+    private static Thread Thread;
 
-        private static ManualResetEvent AcceptEvent;
+    private static ManualResetEvent AcceptEvent;
 
-        public static void Init(string host, int port)
-        {
-            ActiveConnections = new List<Connection>();
+    public static void Init(string host, int port) {
+      ActiveConnections = new List < Connection > ();
 
-            Socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            Socket.Bind(new IPEndPoint(IPAddress.Parse(host), port));
-            Socket.Listen(100);
+      Socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+      Socket.Bind(new IPEndPoint(IPAddress.Parse(host), port));
+      Socket.Listen(100);
 
-            AcceptEvent = new ManualResetEvent(false);
+      AcceptEvent = new ManualResetEvent(false);
 
-            Thread = new Thread(TCPGateway.Update);
-            Thread.Start();
+      Thread = new Thread(TCPGateway.Update);
+      Thread.Start();
 
-            Logger.Print($"TCP Server started at {host}:{port}");
-        }
-
-        private static void Update()
-        {
-            while (true)
-            {
-                AcceptEvent.Reset();
-                Socket.BeginAccept(new AsyncCallback(OnAccept), null);
-                AcceptEvent.WaitOne();
-            }
-        }
-
-        private static void OnAccept(IAsyncResult ar)
-        {
-            try
-            {
-                Socket client = Socket.EndAccept(ar);
-                Connection connection = new Connection(client);
-                ActiveConnections.Add(connection);
-                Logger.Print("New connection!");
-                Connections.AddConnection(connection);
-                client.BeginReceive(connection.ReadBuffer, 0, 1024, SocketFlags.None, new AsyncCallback(OnReceive), connection);
-            } 
-            catch (Exception)
-            {
-                ;
-            }
-
-            AcceptEvent.Set();
-        }
-
-        private static void OnReceive(IAsyncResult ar)
-        {
-            Connection connection = (Connection)ar.AsyncState;
-            if (connection == null) return;
-
-            try
-            {
-                int r = connection.Socket.EndReceive(ar);
-                if (r <= 0)
-                {
-                    Logger.Print("client disconnected.");
-                    ActiveConnections.Remove(connection);
-                    if (connection.MessageManager.HomeMode != null)
-                    {
-                        Sessions.Remove(connection.Avatar.AccountId);
-                    }
-                    connection.Close();
-                    return;
-                }
-
-                connection.Memory.Write(connection.ReadBuffer, 0, r);
-                if (connection.Messaging.OnReceive() != 0)
-                {
-                    ActiveConnections.Remove(connection);
-                    if (connection.MessageManager.HomeMode != null)
-                    {
-                        Sessions.Remove(connection.Avatar.AccountId);
-                    }
-                    connection.Close();
-                    Logger.Print("client disconnected.");
-                    return;
-                }
-                connection.Socket.BeginReceive(connection.ReadBuffer, 0, 1024, SocketFlags.None, new AsyncCallback(OnReceive), connection);
-            }
-            catch (SocketException)
-            {
-                ActiveConnections.Remove(connection);
-                if (connection.MessageManager.HomeMode != null)
-                {
-                    Sessions.Remove(connection.Avatar.AccountId);
-                }
-                connection.Close();
-                Logger.Print("client disconnected.");
-            }
-            catch (Exception exception)
-            {
-                connection.Close();
-                Logger.Print("Unhandled exception: " + exception + ", trace: " + exception.StackTrace);
-            }
-        }
-
-        public static void OnSend(IAsyncResult ar)
-        {
-            try
-            {
-                Socket socket = (Socket)ar.AsyncState;
-                socket.EndSend(ar);
-            }
-            catch (Exception)
-            {
-                ;
-            }
-        }
+      Logger.Print($"TCP Server started at {host}:{port}");
     }
+
+    private static void Update() {
+      while (true) {
+        AcceptEvent.Reset();
+        Socket.BeginAccept(new AsyncCallback(OnAccept), null);
+        AcceptEvent.WaitOne();
+      }
+    }
+
+    private static void OnAccept(IAsyncResult ar) {
+      try {
+        Socket client = Socket.EndAccept(ar);
+        Connection connection = new Connection(client);
+        ActiveConnections.Add(connection);
+        string ipAddress = client.RemoteEndPoint.ToString();
+        Logger.Print($"Client {ipAddress} connected!");
+        Connections.AddConnection(connection);
+        client.BeginReceive(connection.ReadBuffer, 0, 1024, SocketFlags.None, new AsyncCallback(OnReceive), connection);
+      } catch (Exception) {
+        ;
+      }
+
+      AcceptEvent.Set();
+    }
+
+    private static void OnReceive(IAsyncResult ar) {
+      Connection connection = (Connection) ar.AsyncState;
+      if (connection == null) return;
+
+      try {
+        int r = connection.Socket.EndReceive(ar);
+        if (r <= 0) {
+          string ipAddress = connection.Socket.RemoteEndPoint.ToString();
+          Logger.Print($"Client {ipAddress} disconnected.");
+          ActiveConnections.Remove(connection);
+          if (connection.MessageManager.HomeMode != null) {
+            Sessions.Remove(connection.Avatar.AccountId);
+          }
+          connection.Close();
+          return;
+        }
+
+        connection.Memory.Write(connection.ReadBuffer, 0, r);
+        if (connection.Messaging.OnReceive() != 0) {
+          string ipAddress = connection.Socket.RemoteEndPoint.ToString();
+          ActiveConnections.Remove(connection);
+          if (connection.MessageManager.HomeMode != null) {
+            Sessions.Remove(connection.Avatar.AccountId);
+          }
+          connection.Close();
+          Logger.Print($"Client {ipAddress} disconnected.");
+          return;
+        }
+        connection.Socket.BeginReceive(connection.ReadBuffer, 0, 1024, SocketFlags.None, new AsyncCallback(OnReceive), connection);
+      } catch (SocketException) {
+        string ipAddress = connection.Socket.RemoteEndPoint.ToString();
+        ActiveConnections.Remove(connection);
+        if (connection.MessageManager.HomeMode != null) {
+          Sessions.Remove(connection.Avatar.AccountId);
+        }
+        connection.Close();
+        Logger.Print($"Client {ipAddress} disconnected.");
+      } catch (Exception exception) {
+        connection.Close();
+        Logger.Print("Unhandled exception: " + exception + ", trace: " + exception.StackTrace);
+      }
+    }
+
+    public static void OnSend(IAsyncResult ar) {
+      try {
+        Socket socket = (Socket) ar.AsyncState;
+        socket.EndSend(ar);
+      } catch (Exception) {
+        ;
+      }
+    }
+  }
 }
